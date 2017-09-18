f89aca5c7c7f1bb7d850be4fbda29e0c79105c6f
Sascha Wiswedel <mail@sascha-wiswedel.de>
2017-09-18 23:16:59 +0200

    xpath functionality
    added xpath functionality and an example expression

--- website_alert.php
+++ website_alert.php
@@ -8,6 +8,8 @@
 	
 	Note the following arguments the script is expecting.
 	
+	example: php website_alert.php to@example.com from@example.com Example https://www.example.com push positives '//div[contains(@class, "example")]'
+	
 	Initially written by Sascha Wiswedel 
 		berb@sascha-wiswedel.de
 		https://github.com/wiswedel/
@@ -20,9 +22,18 @@ $site = $argv[3]; 		// project identifier, must be 1 word, no spaces
 $url = $argv[4];		// URL to watch, including http(s)
 $push = $argv[5];		// set to "push" to receive push notifications >> requires a token from https://pushme.jagcesar.se/ >> token must be stored inside pushToken.txt
 $negatives = $argv[6]; 	// [positives|negatives] especially for testing purposes, set to "negative" to get an alert every time
+$xpath = $argv[7];		// [noxpath|(xpath-expression in single quotes)] apply an xpath onto the crawled website before diffing
+
 
 // crawl a website and save it as current.txt
-exec("wget -O current_$site.txt $url -q --user-agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'");
+if (isset($xpath) && $xpath !== 'noxpath' && $xpath !== NULL) {
+	
+		exec("wget -q -O - $url -q --user-agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'| xmllint --nowarning --html --xpath '$xpath' - >current_$site.txt 2>/dev/null");
+	
+	} else {
+		
+		exec("wget -O current_$site.txt $url -q --user-agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'");		
+	}
 
 // compare the last crawl with this one and write the result into a variable
 $diff = exec("diff current_$site.txt before_$site.txt");
